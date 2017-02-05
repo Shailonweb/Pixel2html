2c25b9e24030078f9e88bb884e68ebef73189ddf
Shailendra <shailonweb@gmail.com>
2017-02-05 13:00:10 +0530

    responsive

---
+++ javascript/custom.js
@@ -0,0 +1,39 @@
+ $(document).ready(function () {
+                var height = window.innerHeight;
+                var width = window.innerWidth;
+                //alert(width);
+var options = { direction: 'left' };
+	var effect = 'slide';
+	var duration = 500;
+	$(".navbar-toggle").click(function(e){
+		e.stopPropagation();
+    // Set the options for the effect type chosen
+    
+
+    // Set the duration (default: 400 milliseconds)
+    
+		$("#navbar").toggle(effect, options, duration);
+		
+		$('html').toggleClass('killscroll');
+		
+		})
+if(width<768)
+{
+$(document).click(function(){
+	$('html').removeClass('killscroll');
+	$("#navbar").hide(effect, options, duration);
+	});
+}
+else
+{
+	window.resize(function(){
+		
+		$(document).click(function(){
+	$('html').removeClass('killscroll');
+	$("#navbar").hide();
+	});
+		
+		});
+	
+}
+});	
