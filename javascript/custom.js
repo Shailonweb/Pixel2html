fd2aa8a0a11cb9d520b769ef49eb60b92a920e65
Shailendra <shailonweb@gmail.com>
2017-02-06 13:08:49 +0530

    slider update

--- javascript/custom.js
+++ javascript/custom.js
@@ -50,3 +50,65 @@ $("#scrolltop").click(function(){
     return false; 
 	});
 });	
+
+
+var spinner = {
+    index: 0,
+    auto: function(currentIndex) {
+		
+      if (currentIndex != undefined) {
+        spinner.index = currentIndex % spinner.quotes.length;
+      } else {
+        spinner.index = (spinner.index + 1) % spinner.quotes.length;
+      }
+      spinner.quotes.removeClass("active-box");
+      $(spinner.quotes[spinner.index]).addClass("active-box");
+      spinner.dots.removeClass('active_item');
+      $(spinner.dots[spinner.index]).addClass('active_item');
+	  //$('.slider-wrap').height($('.active-box').height());
+    },
+
+    initial: function(){
+		
+		
+      this.quotes = $(".item");
+      this.images = $(".quote-image");
+      spinner.quotes.first().addClass("active-box");
+      //dots
+      for (i = 0; i < spinner.quotes.length; i++) {
+        $('.nav-dots').append('<li class="nav-dot"></div>');
+      }
+      this.dots = $(".nav-dot");
+      $(spinner.dots).first().addClass('active_item');
+	  
+    },
+
+    dotnav: function(){
+      $(spinner.dots).on("click", function(e){
+		 
+        var currentIndex = $(spinner.dots).index(this);
+        spinner.auto(currentIndex);
+        window.clearInterval(interval);
+        interval = window.setInterval(spinner.auto, 6500);
+		
+      });
+    },
+	btn: function(){
+      $('.slide_navigation').on("click", function(e){
+		 
+        var currentIndex = $(spinner.dots).next.index(spinner.dots);
+        spinner.auto(currentIndex);
+        window.clearInterval(interval);
+        interval = window.setInterval(spinner.auto, 6500);
+		
+      });
+    }
+  }
+
+$(document).ready(function() {
+  spinner.initial();
+  spinner.dotnav();
+  spinner.btn();
+  interval = window.setInterval(spinner.auto, 3000);
+ 
+});
